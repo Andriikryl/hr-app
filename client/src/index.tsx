c261b9fcd41f28b95ffeea19f0804b3c2833e47f
Andriikryl <andreyk557@gmail.com>
2023-05-15 19:47:26 +0300

    feat[add routes]

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,24 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
+import { Paths } from "./paths";
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Employees</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <h1>Login</h1>,
+  },
+  {
+    path: Paths.register,
+    element: <h1>Register</h1>,
+  },
+]);
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +28,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>hello world</h1>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
