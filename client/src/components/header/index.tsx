f66ebcb41cd568a63596b5f59f00ec7c96463560
Andriikryl <andreyk557@gmail.com>
2023-05-16 19:09:53 +0300

    feat[header-component]

---
+++ client/src/components/header/index.tsx
@@ -0,0 +1,15 @@
+
+import {Button, Layout, Space, Typography} from "antd";
+import styles from "./index.module.css";
+import { TeamOutlined } from "@ant-design/icons";
+
+export const Header = () => {
+  return (
+    <Layout.Header className={styles.header}>
+        <Space>
+            <TeamOutlined className={styles.teamIcon}/>
+            <Button type="link">Click</Button>
+        </Space>
+    </Layout.Header>
+  );
+};
